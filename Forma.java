f9dc664b261abd58f35daceda3ce76e717b93a6b
JGCarrillo <yosoyjor@gmail.com>
2020-03-18 17:35:00 +0100

    Update Forma.java

--- Forma.java
+++ Forma.java
@@ -1,22 +1,8 @@
-package ejercicio20_poo_abstractas;
-
 abstract class Forma {
 	
 	public Forma(String color) {
 		this.color = color;
 	}
-	
-	/* Para evitarnos el problema con el ejercicio 19 al invocar a un metod perteneciente a la instancia Monstruo
-	 * atacar() el cual todavia no estaba definido, usamos las clases abstractas, en las cuales SOLO definimos 
-	 * el metodo que queremos que implementen las subclases, no lo desarrollamos. Tanto la clase como los métodos
-	 * deberan ser abstractos
-	 * 
-	 * UNA CLASE ABSTRACTA NO PUEDE SER INSTANCIADA. Para usar una clase abstracta tienes que 
-	 * derivar a una subclase desde la clase abstracta, sobreescribi el metodo abstracto y
-	 * darle una funcionalidad
-	 * 
-	 * Es decir, se podra hacer --> Forma triangulo = new Triangulo()
-	 */
 	
 	// --- . ---- . --- . --- METODOS --- . ---- . --- . --- *
 	public abstract double getArea();
